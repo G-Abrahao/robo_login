af7e8e1aeae5cea000868088fcbc42a91c365d23
G-Abrahao <abrahaog67@gmail.com>
2023-09-04 12:29:15 -0300

    main

---
+++ robo_login/main.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://ava.mg.senac.br/edu/")
+        page.fill('xpath=//*[@id="username"]', "seu_usuario@senacminas.edu.br") 
+        page.locator('xpath=//*[@id="boxForm"]/div/form/div[3]/button').click()
+        page.wait_for_timeout(60000)
+main()
